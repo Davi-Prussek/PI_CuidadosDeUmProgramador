a3affa443932de921cf69159921d87e0b6593a75
davi-prussek <davi.mariano.prussek0916@gmail.com>
2025-09-26 02:33:36 -0300

    Arrumando, sincronizando e adicionando MUITA coisa

---
+++ js/theme.js
@@ -0,0 +1,48 @@
+const HTML = document.getElementById("html");
+const body = document.getElementById("body");
+const main = document.getElementById("main");
+const header = document.getElementById("header");
+const footer = document.getElementById("footer");
+const setaVoltar = document.getElementById("setaVoltar");
+
+const botaoOff = document.getElementById("themeButtonOff");
+const botaoOn = document.getElementById("themeButtonOn")
+
+const ligadoDesligado = document.getElementById("desligado");
+
+window.onload = () => {
+    const tema = localStorage.getItem("theme");
+    if (tema == "light") {
+        light();
+    } else {
+        dark();
+    }
+}
+const ligaDesliga = () => {
+    const atual = localStorage.getItem("theme");
+    if (atual == "light") {
+        dark();
+    } else {
+        light();
+    }   }   
+
+function light() {
+    botaoOn.style.display = "none";
+    botaoOff.style.display = "block";
+    header.style.backgroundColor = "rgb(0, 0, 0)";
+    setaVoltar.style.backgroundColor = "rgb( 0, 0, 0)";
+    footer.style.background = "linear-gradient( rgba(23, 23, 23, 1), rgba(18, 18, 18, 1))";
+    localStorage.setItem("theme", "light");
+}
+
+function dark() {
+    botaoOff.style.display = "none";
+    botaoOn.style.display = "block";
+    header.style.backgroundColor = "#0056b3";
+    setaVoltar.style.backgroundColor = "#0056b3";
+    footer.style.background = "linear-gradient( #0057b3c4, #002457)";
+    localStorage.setItem("theme", "dark");
+}
+
+botaoOff.addEventListener("click", ligaDesliga);
+botaoOn.addEventListener("click", ligaDesliga);
