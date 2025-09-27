4d38e82f93f198fdd286577e07b29809fdcc0564
davi-prussek <davi.mariano.prussek0916@gmail.com>
2025-09-27 19:50:41 -0300

    Fazendo tema escuro

--- js/theme.js
+++ js/theme.js
@@ -1,9 +1,22 @@
-const HTML = document.getElementById("html");
-const body = document.getElementById("body");
-const header = document.getElementById("header");
-const footer = document.getElementById("footer");
-const setaVoltar = document.getElementById("setaVoltar");
+const HTML = document.querySelector("html");
+const body = document.querySelector("body");
 
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+const main = document.querySelector("main");
+
+const section = document.querySelectorAll("section");
+const div = document.querySelectorAll("div");
+const p = document.querySelectorAll("p");
+const h1 = document.querySelectorAll("h1");
+const h2 = document.querySelectorAll("h2");
+const h3 = document.querySelectorAll("h3");
+const h4 = document.querySelectorAll("h4");
+const h5 = document.querySelectorAll("h5");
+const h6 = document.querySelectorAll("h6");
+const span = document.querySelectorAll("span");
+
+const setaVoltar = document.getElementById("setaVoltar");
 
 const botaoOff = document.getElementById("themeButtonOff");
 const botaoOn = document.getElementById("themeButtonOn")
@@ -32,6 +45,12 @@ function light() {
     header.style.backgroundColor = "rgb(0, 0, 0)";
     setaVoltar.style.backgroundColor = "rgb( 0, 0, 0)";
     footer.style.background = "linear-gradient( rgba(23, 23, 23, 1), rgba(18, 18, 18, 1))";
+    main.style.backgroundColor = "rgba(19, 19, 19, 1)";
+    HTML.style.backgroundColor = "rgba(16, 16, 16, 1)";
+    section.forEach(s => {s.style.color = "#ffffff";});
+    div.forEach(d => { d.style.color = "#ffffff"; });
+    div.forEach(d => { d.style.backgroundColor = "black"; });
+    p.forEach(p => {p.style.color = "#ffffff"});
     localStorage.setItem("theme", "light");
 }
 
@@ -41,6 +60,12 @@ function dark() {
     header.style.backgroundColor = "#0056b3";
     setaVoltar.style.backgroundColor = "#0056b3";
     footer.style.background = "linear-gradient( #0057b3c4, #002457)";
+    main.style.backgroundColor = "";
+    HTML.style.backgroundColor = "";
+    section.forEach(s => {s.style.color = "";});
+    div.forEach(d => { d.style.color = ""; });
+    div.forEach(d => { d.style.backgroundColor = ""; });
+    p.forEach(p => {p.style.color = ""});
     localStorage.setItem("theme", "dark");
 }
 
